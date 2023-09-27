8882b6725d6fec8f03a93c9cc66f25684becb8dd
Hyungjun Rho <hjworks20@gmail.com>
2023-09-28 01:06:52 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,9 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hjworks', // Usually your GitHub org/user name.
   projectName: 'hjworks.github.io', // Usually your repo name.
+  trailingSlash: false,
   deploymentBranch: 'gh-pages',
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
