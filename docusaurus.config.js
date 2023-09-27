3d6853f995f6a1e2a1eaec94a1152ff1d1873f44
HJ Works <zuntopia@gmail.com>
2023-09-28 00:31:12 +0900

    add deploy branch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hjworks', // Usually your GitHub org/user name.
   projectName: 'hjworks.github.io', // Usually your repo name.
-
+  deploymentBranch: 'gh-pages',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
